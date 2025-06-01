6f54403ce168865cb39d03f7782c2e6f42bf457d
Stirling Reynolds <stirlo@touch.asn.au>
2025-06-01 14:11:07 +1000

    Refactor code style for consistency and add forecast view to handle API requests

--- amber_client/apps.py
+++ amber_client/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AmberClientConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'amber_client'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "amber_client"
--- amber_client/views.py
+++ amber_client/views.py
@@ -1,3 +1,17 @@
+from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def forecast(request):
+    res = requests.get(
+        f"https://api.amber.com.au/v1/sites/{settings.AMBER_LOCATION_ID}/prices/current",
+        params={"next": 12, "previous": 0},
+        headers={"Authorization": f"Bearer {settings.AMBER_API_KEY}"},
+    )
+    res.raise_for_status()
+
+    data = res.json()
+
+    return JsonResponse(data, safe=False)
